d80140510c4a5c1456668497fe757d387bd95dd2
tobimadehin <tobimadehin@gmail.com>
2025-10-19 16:50:52 +0100

    Non static port assignment

--- php/server.php
+++ php/server.php
@@ -1,9 +1,8 @@
 <?php
 
-$port = 3000;
-$host = 'localhost';
-$documentRoot = __DIR__;
-
+$port = $_ENV['PORT'] ?? 3000;
+$host = $_ENV['HOST'] ?? 'localhost';
+$documentRoot = $_ENV['DOCUMENT_ROOT'] ?? __DIR__;
 echo "Old County Times PHP server starting on http://$host:$port\n";
 echo "Document root: $documentRoot\n";
 echo "Press Ctrl+C to stop the server\n\n";
